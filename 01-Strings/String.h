66cf0c80f24dec4c2665152bfd65d9f48ae30ff8
LucianoKapp <Lucianokapp1@gmail.com>
2026-05-01 16:43:31 -0300

    Corregido Pre y post

--- 01-Strings/String.h
+++ 01-Strings/String.h
@@ -3,7 +3,22 @@
 #define STRING_H
 #include<stdbool.h>
 // los strings pueden ser char* o char[]
+/**
+ * Verifica si una cadena está vacía
+ * Precondición: string != NULL
+ * Postcondición: retorna 1 si la cadena es "", 0 en caso contrario
+ */
 int IsEmpty(const char * );  // 0 False 1 True  o la hacemos tipo bool y usamos <stdbool.h>
-int GetLength(const char*);  
+/**
+ * Obtiene la longitud de una cadena
+ * Precondición: string != NULL
+ * Postcondición: retorna la cantidad de caracteres (sin contar '\0')
+ */
+int GetLength(const char*);
+   /**
+ * Compara dos cadenas
+ * Precondición: string1 != NULL && string2 != NULL
+ * Postcondición: retorna true si tienen el mismo contenido, false si difieren
+ */
 bool AreEqual(const char*cadena1, char*cadena2);
 #endif 
